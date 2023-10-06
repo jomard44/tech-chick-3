90f776ebaf8a4bb5a286202c6d20ae12af19d191
jomard44 <jomrdali44@gmail.com>
2023-10-06 09:03:24 -0300

    added if else

---
+++ script.js
@@ -0,0 +1,40 @@
+/*
+author: jomard
+date: october/6/2023
+descption: tech chick 3
+*/
+
+// DO NOT EDIT: The main function to house our program code 
+function main()
+{
+    //input
+    let a = 4
+    let b = 3
+    let c = 2
+    let d = 1
+    let f = 0
+    //process
+    if(a){
+        console.log(4)
+
+    }else if(b){
+        console.log(3)
+    }else if(c){
+        console.log(2)
+    }else if(d){
+        console.log(1)
+    }else if(f){
+        console.log(0)
+    }else{
+        console.log("the info you enterd is wrong")
+    }
+
+    //output
+
+}
+
+// DO NOT EDIT: Trigger our main function to launch the program
+if (require.main === module)
+{
+    main();
+}
